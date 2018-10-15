0a61a76a505efcb1ea8e308e372fb356eca23203
rameesraja <rameesraja1992@gmail.com>
2018-10-15 23:12:36 +0530

    visualization

--- notebooks/Users/rameesr@visualbi.com/Iris Data.py
+++ notebooks/Users/rameesr@visualbi.com/Iris Data.py
@@ -50,3 +50,67 @@ ax[1].set(title='Petal Comparission')
 display(fig)
 
 # Basic comparssions Sepal length Vs Width | Petal Length vs Width
+#Clearly there are two separate clusters here. But we dont know what is what.
+
+# COMMAND ----------
+
+#Now plotting each of the species separately to understand the distribution.
+
+plt.figure()
+fig,ax=plt.subplots(1,2,figsize=(15,10))
+pdata[pdata['species']=='setosa'].plot(x="sepal_length",y="sepal_width",kind="scatter",ax=ax[0],label="setosa",color='g')
+pdata[pdata['species']=='virginica'].plot(x="sepal_length",y="sepal_width",kind="scatter",ax=ax[0],label="virginica",color='b')
+pdata[pdata['species']=='versicolor'].plot(x="sepal_length",y="sepal_width",kind="scatter",ax=ax[0],label="versicolor",color='r')
+pdata[pdata['species']=='setosa'].plot(x="petal_length",y="petal_width",kind="scatter",ax=ax[1],label="setosa",color='g')
+pdata[pdata['species']=='virginica'].plot(x="petal_length",y="petal_width",kind="scatter",ax=ax[1],label="virginica",color='b')
+pdata[pdata['species']=='versicolor'].plot(x="petal_length",y="petal_width",kind="scatter",ax=ax[1],label="versicolor",color='r')
+#print(pdata)
+display(fig)
+
+# Sepal
+#Setosa has separate cluster in sepal length vs width and there is a linear relationship
+#Its tough to differentiate virginica and versicolor
+
+#Petal
+#Again Sepal forms a separate cluster.
+# virginica lies on the higher side when compared to versicolor.
+
+
+
+# COMMAND ----------
+
+#Lets check the distribution overlaps
+
+plt.figure()
+fig,ax = plt.subplots(2,2,figsize=(15,15))
+pdata[pdata['species']=='setosa'].sepal_length.plot(kind="hist",ax=ax[0][0],label="setosa",color='g',fontsize=10,alpha=0.2,histtype='step',stacked=True,fill=True)
+pdata[pdata['species']=='virginica'].sepal_length.plot(kind="hist",ax=ax[0][0],label="virginica",color='b',fontsize=10,alpha=1,histtype='step',stacked=True,fill=False)
+pdata[pdata['species']=='versicolor'].sepal_length.plot(kind="hist",ax=ax[0][0],label="versicolor",color='r',fontsize=10,alpha=0.1,histtype='step',stacked=True,fill=True)
+ax[0][0].legend(prop={'size': 10})
+ax[0][0].set(title='Sepal Length frequency')
+#ax[0][0].hist(pdata[pdata['species']=='setosa'].sepal_length,bins='auto',alpha=0.5)
+############
+pdata[pdata['species']=='setosa'].sepal_width.plot(kind="hist",ax=ax[0][1],label="setosa",color='g',fontsize=10,alpha=0.2,histtype='step',stacked=True,fill=True)
+pdata[pdata['species']=='virginica'].sepal_width.plot(kind="hist",ax=ax[0][1],label="virginica",color='b',fontsize=10,alpha=1,histtype='step',stacked=True,fill=False)
+pdata[pdata['species']=='versicolor'].sepal_width.plot(kind="hist",ax=ax[0][1],label="versicolor",color='r',fontsize=10,alpha=0.1,histtype='step',stacked=True,fill=True)
+ax[0][1].legend(prop={'size': 10})
+ax[0][1].set(title='Sepal Width frequency')
+
+#############
+
+pdata[pdata['species']=='setosa'].petal_length.plot(kind="hist",ax=ax[1][0],label="setosa",color='g',fontsize=10,alpha=0.2,histtype='step',stacked=True,fill=True)
+pdata[pdata['species']=='virginica'].petal_length.plot(kind="hist",ax=ax[1][0],label="virginica",color='b',fontsize=10,alpha=1,histtype='step',stacked=True,fill=False)
+pdata[pdata['species']=='versicolor'].petal_length.plot(kind="hist",ax=ax[1][0],label="versicolor",color='r',fontsize=10,alpha=0.1,histtype='step',stacked=True,fill=True)
+ax[1][0].legend(prop={'size': 10})
+ax[1][0].set(title='petal Length frequency')
+####################
+pdata[pdata['species']=='setosa'].petal_width.plot(kind="hist",ax=ax[1][1],label="setosa",color='g',fontsize=10,alpha=0.2,histtype='step',stacked=True,fill=True)
+pdata[pdata['species']=='virginica'].petal_width.plot(kind="hist",ax=ax[1][1],label="virginica",color='b',fontsize=10,alpha=1,histtype='step',stacked=True,fill=False)
+pdata[pdata['species']=='versicolor'].petal_width.plot(kind="hist",ax=ax[1][1],label="versicolor",color='r',fontsize=10,alpha=0.1,histtype='step',stacked=True,fill=True)
+ax[1][1].legend(prop={'size': 10})
+ax[1][1].set(title='petal Width frequency')
+
+display(fig)
+
+# Sepal length and width are having  overlapping values across species.
+# Petal length and width are distinct for setosa and slight overlap between other two
